6d7dcc96229ff7630341a9370553f7946fa127fd
aneeshdurg <durg2@illinois.edu>
2015-10-07 16:14:24 -0500

    added static_root and staticfiles_dirs to settings.py

--- memorylane/memorylane/settings.py
+++ memorylane/memorylane/settings.py
@@ -100,4 +100,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT =   os.path.join(PROJECT_DIR, "static")
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+
+    ('assets', os.path.join(PROJECT_DIR, '../static')),
+)
